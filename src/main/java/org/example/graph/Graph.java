133440ff3178021091e32d080842b6ae585907ed
arturbernardo <artur.bernardo.k@gmail.com>
2024-03-05 18:30:46 -0300

    init Graphs

--- src/main/java/org/example/graph/Graph.java
+++ src/main/java/org/example/graph/Graph.java
@@ -15,6 +15,15 @@ public class Graph {
         return false;
     }
 
+    public boolean removeVertex(String vertex) {
+        if (adjList.get(vertex) == null) return false;
+        for (String otherVertex : adjList.get(vertex)) {
+            adjList.get(otherVertex).remove(vertex);
+        }
+        adjList.remove(vertex);
+        return true;
+    }
+
     public boolean removeEdge(String vertex1, String vertex2) {
         if (adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
             adjList.get(vertex1).remove(vertex2);
